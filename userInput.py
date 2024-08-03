2a38f7339234174f419124311d30e0e7e6c96e78
michaeljumah <michaeljumah8@gmail.com>
2024-08-03 21:08:09 +0300

    a program that changes the europe lift convention to that of USA

--- userInput.py
+++ userInput.py
@@ -1,4 +1,8 @@
+#---------------------------------------------------------------
 #a program that changes the europe lift convention to that of USA
 inp = input("what is your Europe lift destination: ") 
 UsDest = int(inp) + 1
 print("Your destination is: ", UsDest)
+
+
+#---------------------------------------------------------------
