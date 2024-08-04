18af4bb0ee0c64fb1e121d263c37f56120ea7a73
michaeljumah <michaeljumah8@gmail.com>
2024-08-04 21:48:04 +0300

    try and except error handlers

--- userInput.py
+++ userInput.py
@@ -1,8 +1,14 @@
 #---------------------------------------------------------------
 #a program that changes the europe lift convention to that of USA
-inp = input("what is your Europe lift destination: ") 
-UsDest = int(inp) + 1
+inp = input("what is your Europe lift destination: ")
+try:
+    UsDest = int(inp) + 1
+        
+except:
+    UsDest = -1
+    
 print("Your destination is: ", UsDest)
-
-
-#---------------------------------------------------------------
+if UsDest == -1:
+    print("Wrong Value")
+    
+#------------------------------------------------------
