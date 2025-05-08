5c314a97d8c43924981c66d5d2580c7cf24b6911
michaeljumah <michaeljumah8@gmail.com>
2025-05-08 14:06:27 +0300

    for loops

---
+++ tutorial.py
@@ -0,0 +1,6 @@
+'''For Statement
+python's for statement iterates over the items of any sequence (a list or a string), in the order that they appear in the sequence.
+'''
+words = ['cat', 'window', 'defenestrate', 'demonstrate']
+for w in words:
+    print(w, len(w))
